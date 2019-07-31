9daefe30dabb2de267fdc3a216f078bbf364feb0
zhoujie <673191158@qq.com>
2019-07-31 17:32:03 +0800

    正式服打包

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,7 +3,7 @@
 // let HOST = process.argv.splice(2)[0];
 module.exports = {
     NODE_ENV: '"production"',
-    BASE_API: '"http://tdxcx.wuhanlst.com:8899"'  //  预发布
+    BASE_API: '"http://tdxcx.whtdzyy.com:8899"'  //  预发布
     // BASE_API: '"https://apitest.wuhanlst.com:8013"' //  正式地址
     // BASE_API: '"'+HOST+'"'
 }
