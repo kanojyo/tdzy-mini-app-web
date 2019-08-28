922f7455c78d45a47fab5d0e482dfec4ef6113dc
zhoujie <673191158@qq.com>
2019-08-28 11:34:47 +0800

    提交代码

--- src/views/Office/office.vue
+++ src/views/Office/office.vue
@@ -50,12 +50,12 @@
                             <div v-if="scope.row.state === 2">
                                 <span class="cursor color-f8494c" v-if="menuData.start_stop " @click="statusChange(scope.row.id, 1)">上架</span>
                                 <span class="cursor color-f8494c" v-if="menuData.edit" @click="edit(scope.row.id)">编辑</span>
-                                <span class="cursor color-f8494c" v-if="menuData.review" @click="preview(scope.row.office_details)">科室预览</span>
+                                <span class="cursor color-f8494c" v-if="menuData.review" @click="preview(scope.row.id)">科室预览</span>
                             </div>
                             <div v-if="scope.row.state === 1">
                                 <span class="cursor color_red" v-if="menuData.start_stop" @click="statusChange(scope.row.id, 2)">下架</span>
                                 <span class="cursor color-f8494c" v-if="menuData.edit" @click="edit(scope.row.id)">编辑</span>
-                                <span class="cursor color-f8494c" v-if="menuData.review" @click="preview(scope.row.office_details)">科室预览</span>
+                                <span class="cursor color-f8494c" v-if="menuData.review" @click="preview(scope.row.id)">科室预览</span>
                             </div>
                         </template>
                     </el-table-column>
@@ -404,10 +404,14 @@ export default {
         },
         async preview(val){    //  打开医生预览
             this.previewImgList='';
-            if(val !== ''){
-                this.previewImgList=JSON.parse(val);
+            let data = await officeDetail({id: val});
+            if(data.code == 200){
+                this.previewImgList = data.data.office_details;
+                if(this.previewImgList !== ''){
+                    this.previewImgList=JSON.parse(this.previewImgList);
+                }
+                this.previewShow=true;
             }
-            this.previewShow=true;
         },
         search() {
             //  检索
